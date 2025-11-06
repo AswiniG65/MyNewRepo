5e4d6bf9853cf65fe4d91b85d4893039f30863ee
AswiniG65 <gollapalleaswini@gmail.com>
2025-11-06 19:33:38 +0530

    Update app.java

--- app.java
+++ app.java
@@ -4,3 +4,22 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 }
+
+// dev-2 push from local 
+public class CallingMethodsInSameClass
+{
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+}
